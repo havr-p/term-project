c9735347d515d6a90e4c9a9a2cfea94e02c527dc
havr-p <78024475+havr-p@users.noreply.github.com>
2022-01-23 21:20:12 +0100

    rewrote silly recursive factorial

--- src/test/java/graphs/BacktrackingFlowTest.java
+++ src/test/java/graphs/BacktrackingFlowTest.java
@@ -38,8 +38,9 @@ public class BacktrackingFlowTest {
     List<List<Map<Integer, Integer>>> flows = new ArrayList<>();
 
     public int factorial(int n) {
-        if (n == 0) return 1;
-        return n * factorial(n - 1);
+       int fact = 1;
+       for (int i = 2; i <= n; i++) fact*=i;
+       return fact;
     }
 
     @Test
